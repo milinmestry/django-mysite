a3965fa6f4a238c346142982deac82da3dafbc28
Milin Mestry <milinmestry@gmail.com>
2017-04-19 00:02:45 +0530

    sudo apt-get install libmysqlclient-dev | pip install mysqlclient

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'mysite_db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'mysite_db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-polls',
+        'USER': 'user_read_write',
+        'PASSWORD': 'ReadUserWrite',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
